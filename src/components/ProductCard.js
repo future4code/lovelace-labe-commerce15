74154b134e36075a1cda9ee653cf7a0ace53f3a4
Felipe <fegushiken@gmail.com>
2021-07-22 11:14:36 -0300

    Commit das 11h

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 class ProductCard extends React.Component {
     render() {
         return (
-        
+          <h1>Olá</h1>
       )
     }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 class Products extends React.Component {
     render() {
         return (
-        
+          <h1>Olá</h1>
       )
     }
 }
