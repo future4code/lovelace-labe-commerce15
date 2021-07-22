2ac9db4d6208a73b0748ae83a92c2904e3b1bd19
LuisMijias <luismijias@gmail.com>
2021-07-22 16:54:10 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -1,59 +1,159 @@
-import React from 'react';
-import styled from 'styled-components';
-import Products from './components/Products';
-import Filters from './components/Filters';
-import Home from './components/Home';
-import ShoppingCart from './components/ShoppingCart';
-import ShoppingCartItem from './components/ShoppingCartItem';
+import React from "react";
+import { Filters } from "./components/Filters/Filters";
+import { Products } from "./components/Products/Products";
+import { ShoppingCart } from "./components/ShoppingCart/ShoppingCart";
+import styled from "styled-components";
+import { Title } from "./components/styles";
 
 
-const Espacamento = styled.div `
-margin: 10px;
-
-`
 
 
+const AppContainer = styled.div`
+  display: ruby-base-container;
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+  
+`;
+
+const products = [
+  {
+    id: 1,
+    name: "Foguete - Apollo XI ",
+    price: 10000,
+    photo: "https://images8.alphacoders.com/105/thumb-1920-1054256.jpg"
+  },
+  {
+    id: 2,
+    name: "Inteligência Artificial by Felipe Gushiken",
+    price: 200,
+    photo: "https://picsum.photos/200/200?a=7"
+  },
+  {
+    id: 3,
+    name: "Produto 3",
+    price: 30,
+    photo: "https://picsum.photos/200/200?a=3"
+  },
+  {
+    id: 4,
+    name: "Produto 4",
+    price: 10,
+    photo: "https://picsum.photos/200/200?a=4"
+  }
+];
 
 class App extends React.Component {
-  state={
-    id: Date.now(),
-    nome: "produto",
-    preco:"100",
-    imagem:"https://picsum.photos/seed/picsum/200/300"   
+  state = {
+    minFilter: -Infinity,
+    maxFilter: Infinity,
+    nameFilter: "",
+    productsInCart: [
+      {
+        id: 4,
+        name: "Produto 4",
+        price: 10,
+        photo: "https://picsum.photos/200/200?a=4",
+        quantity: 1
+      },
+      {
+        id: 3,
+        name: "Produto 3",
+        price: 30,
+        photo: "https://picsum.photos/200/200?a=3",
+        quantity: 2
+      }
+    ]
+  };
 
+  onChangeMinFilter = (event) => {
+    this.setState({ minFilter: event.target.value });
+  };
 
-  }
+  onChangeMaxFilter = (event) => {
+    this.setState({ maxFilter: event.target.value });
+  };
 
+  onChangeNameFilter = (event) => {
+    this.setState({ nameFilter: event.target.value });
+  };
 
+  onAddProductToCart = (productId) => {
+    const productInCart = this.state.productsInCart.find(
+      (product) => productId === product.id
+    );
 
+    if (productInCart) {
+      const newProductsInCart = this.state.productsInCart.map((product) => {
+        if (productId === product.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1
+          };
+        }
 
+        return product;
+      });
+
+      this.setState({ productsInCart: newProductsInCart });
+    } else {
+      const productToAdd = products.find((product) => productId === product.id);
+
+      const newProductsInCart = [
+        ...this.state.productsInCart,
+        { ...productToAdd, quantity: 1 }
+      ];
+
+      this.setState({ productsInCart: newProductsInCart });
+    }
+  };
+
+  onRemoveProductFromCart = (productId) => {
+    const newProductsInCart = this.state.productsInCart
+      .map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            quantity: product.quantity - 1
+          };
+        }
+        return product;
+      })
+      .filter((product) => product.quantity > 0);
+
+    this.setState({ productsInCart: newProductsInCart });
+  };
 
   render() {
     return (
-      <Espacamento> 
-        <Filters>
-
-
-        </Filters>
-        <Home>
-          <Products>
-
-          </Products>
-        </Home>
-        <ShoppingCart>
-          <ShoppingCartItem>
-
-          </ShoppingCartItem>
-
-        </ShoppingCart>
-
-
-
-      </Espacamento>
-    )
-
+      <AppContainer>
+       <h1>lkjlkj</h1>
+       <Title>
+        <Filters
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onChangeMinFilter={this.onChangeMinFilter}
+          onChangeMaxFilter={this.onChangeMaxFilter}
+          onChangeNameFilter={this.onChangeNameFilter}
+        /> <ShoppingCart
+        productsInCart={this.state.productsInCart}
+        onRemoveProductFromCart={this.onRemoveProductFromCart}
+      />
+      </Title>
+        
+        <Products
+        
+          products={products}
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onAddProductToCart={this.onAddProductToCart}
+        />
+       
+      </AppContainer>
+    );
   }
-
 }
 
 export default App;
---
+++ src/components/Products/ProductCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CardInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+
+  p {
+    margin: 4px 0;
+  }
+`;
+
+const AddToCartButton = styled.button`
+  align-self: center;
+  margin-top: 4px;
+  font-style: italic;
+  font-family: Arial, Helvetica, sans-serif;
+  border-radius: 3rem;
+`;
+
+export class ProductCard extends React.Component {
+  render() {
+    const product = this.props.product;
+    return (
+      <CardContainer>
+        <img src={product.photo} />
+        <CardInfo>
+          <p>{product.name}</p>
+          <p>R${product.price},00</p>
+          <AddToCartButton
+            onClick={() => this.props.onAddProductToCart(product.id)}
+          >
+            Adicionar ao carrinho
+          </AddToCartButton>
+        </CardInfo>
+      </CardContainer>
+    );
+  }
+}
